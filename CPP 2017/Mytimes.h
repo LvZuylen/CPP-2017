71604719ef90dc59797467ccd13d82ba04e3926d
Justin van Son <jalson@avans.nl>
2017-02-05 17:43:15 +0100

    hh

--- CPP 2017/Mytimes.h
+++ CPP 2017/Mytimes.h
@@ -10,12 +10,13 @@ public:
 	virtual int getSecs();
 	virtual void tick(); //1 second
 	virtual void add(int hours, int mins, int secs);
-
 	virtual bool equals(Mytimes & time);
-
+	//lucas houdt van mannen
 	virtual void giveTime();
 		
 private:
 	void normalise(); // vb: 61 min 61 sec. ==> 1 h 2 min 1 sec
 	int hours, mins, secs;
-};
+};
+
+
