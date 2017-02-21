92db40ac5e5acc8af5508c91869a43bbe7c76324
Lucas van Zuylen <ln.vanzuylen@student.avans.nl>
2017-02-21 16:16:50 +0100

    map functies added

--- Provider.cpp/Mobiel.h
+++ Provider.cpp/Mobiel.h
@@ -13,13 +13,18 @@ public:
 	~Mobiel();
 	virtual void ontvang(const Sms &sms);
 	virtual void verzend(const std::string &tekst, int naar);
-	virtual std::string mobielInfo() const;
+	virtual const std::string mobielInfo();
 	virtual void addContact(std::string naam, int nr);
+	virtual void showContacts();
+	virtual void removeContact(std::string naam);
+	virtual void editContact(std::string naam, int nr, std::string newNaam, int newNr);
+
+
 //private:
 	std::map<std::string, int> contactLijst;
 	int telnr;
 	Provider *provider;
 	Sms bericht;
-
+	std::map<std::string, int>::iterator it = contactLijst.begin();
 };
 
